f8773b6cf7d98f948a52e596d428b7a6b41350d9
Vicenciomf1 <101673305+Vicenciomf1@users.noreply.github.com>
2023-02-14 22:34:23 -0300

    Fix de bugs

--- src/server/router/carritoRuta.js
+++ src/server/router/carritoRuta.js
@@ -57,10 +57,16 @@ carritoRuta.delete("/:id/productos/:id_prod", async (req, res) => {
   if (!id || !id_prod) res.status(404).json({"error": -3, "descripcion": `carrito (${id}) o producto (${id_prod}) con un ID que no es un número en alguno de los dos`});
 
   const carrito = await contenedorCarrito.getById(id);
-  carrito.productos = carrito.productos.filter((prod) => prod.id !== id_prod);  // Modifica la referencia
+  if (!carrito) res.status(404).json({"error": -3, "descripcion": `carrito (${id}) no encontrado`});
+
+  const indice = carrito.productos.findIndex(prod=>prod.id==id_prod);  // Viene como string, por eso el ==
+  if (indice === -1) res.status(404).json({"error": -3, "descripcion": `producto (${id_prod}) no encontrado en el carrito (${id})`});
+
+  carrito.productos.splice(indice, 1);
   const nuevoCarrito = await contenedorCarrito.updateById(id, carrito);
 
   res.json({ nuevoCarrito });
+  // Duda: ¿Cómo anido los if para que no se pasen a lo siguiente? O es mejor usar un return? Porque el response.algo() no corta el callback handler de la petición
 });
 
 carritoRuta.use((req, res) => res.status(404).json({ error: -2, descripcion: `ruta api/carrito/${req.path} método ${req.method} no implementada` }));
